832a687193a728c0d56ecf53a2a2384e9068e237
colchang <colchang@iu.edu>
2020-09-22 19:11:59 -0400

    fixed bug regarding event handler

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,21 @@ $(document).ready(function(){
             $("#contact-container").css("display", "block");
             $("#intro-container").css("display", "none");
         })
+        $("#resume-container").on("click", function() {
+            $("#resume-container").css("display", "block");
+            $("#portfolio-container").css("display", "block");
+            $("#about-container").css("display", "block");
+            $("#contact-container").css("display", "block");
+            $("#intro-container").css("display", "none");
+        })
         $("#portfolio-toolbar").on("click", function() {
+            $("#portfolio-container").css("display", "block");
+            $("#about-container").css("display", "block");
+            $("#contact-container").css("display", "block");
+            $("#resume-container").css("display", "none");
+            $("#intro-container").css("display", "none");
+        })
+        $("#portfolio-container").on("click", function() {
             $("#portfolio-container").css("display", "block");
             $("#about-container").css("display", "block");
             $("#contact-container").css("display", "block");
@@ -28,9 +42,9 @@ $(document).ready(function(){
             $("#resume-container").css("display", "none");
             $("#intro-container").css("display", "none");
         })
-        $("#contact-toolbar").on("click", function() {
+        $("#about-container").on("click", function() {
+            $("#about-container").css("display", "block");
             $("#contact-container").css("display", "block");
-            $("#about-container").css("display", "none");
             $("#portfolio-container").css("display", "none");
             $("#resume-container").css("display", "none");
             $("#intro-container").css("display", "none");
